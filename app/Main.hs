e775e3cc5b8667d461c6709fbd454d4b8a8bf7cd
Matthew Chan <matt@themattchan.com>
2016-04-23 18:03:47 -0700

    pipeline

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Lib
 
 main :: IO ()
-main = draw example
+main = draw pipeline
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,20 +1,24 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Lib where
 
 import Diagrams.Prelude
 import Diagrams.Backend.PGF.CmdLine
 
-pipeline :: Diagram B
-pipeline = rect 0.10 0.5 ||| rect 0.4 0.5
+pipeline :: Diagram PGF
+pipeline = foldr1 (|||) $ map blk [ "IF", "ID", "EXE", "MEM", "WB" ]
+  where blk name = rect 20 10 <> text name
 
-example :: Diagram B
-example = square 1
-      ||| rect 0.3 0.5
-      ||| triangle 1
-      ||| roundedRect  0.5 0.4 0.1
-      ||| roundedRect  0.5 0.4 (-0.1)
-      ||| roundedRect' 0.7 0.4 (with & radiusTL .~ 0.2
-                                     & radiusTR .~ -0.2
-                                     & radiusBR .~ 0.1)
 
-draw :: Diagram B -> IO ()
-draw d = defaultMain (pad 1.1 d)
+
+-- example :: Diagram PGF
+-- example = square 1
+--       ||| rect 0.3 0.5
+--       ||| triangle 1
+--       ||| roundedRect  0.5 0.4 0.1
+--       ||| roundedRect  0.5 0.4 (-0.1)
+--       ||| roundedRect' 0.7 0.4 (with & radiusTL .~ 0.2
+--                                      & radiusTR .~ -0.2
+--                                      & radiusBR .~ 0.1)
+
+draw :: Diagram PGF -> IO ()
+draw = defaultMain . pad 1.1
