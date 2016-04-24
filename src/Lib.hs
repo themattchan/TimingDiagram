2fb23799de24a34077a1334732ec668275f8aef5
Matthew Chan <matt@themattchan.com>
2016-04-23 18:22:40 -0700

    coloured pipeline

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,24 +1,25 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Lib where
 
+import Control.Applicative
 import Diagrams.Prelude
 import Diagrams.Backend.PGF.CmdLine
 
 pipeline :: Diagram PGF
-pipeline = foldr1 (|||) $ map blk [ "IF", "ID", "EXE", "MEM", "WB" ]
-  where blk name = rect 20 10 <> text name
+pipeline = foldr1 (|||) labelledBlocks
+  where
+    block name color = label name <> (rect 20 10 # fcA color)
+    label name = text name # bold
+                           # fontSize (local 2)
+                           # font "Computer Modern Teletype L"
+                           # fc white
+    colours = map (`withOpacity` 0.75)
+             [ red, orange, green, blue, purple ]
+    stages = [ "IF", "ID", "EXE", "MEM", "WB"   ]
+    labelledBlocks = getZipList $
+                       block <$> ZipList stages <*> ZipList colours
 
 
-
--- example :: Diagram PGF
--- example = square 1
---       ||| rect 0.3 0.5
---       ||| triangle 1
---       ||| roundedRect  0.5 0.4 0.1
---       ||| roundedRect  0.5 0.4 (-0.1)
---       ||| roundedRect' 0.7 0.4 (with & radiusTL .~ 0.2
---                                      & radiusTR .~ -0.2
---                                      & radiusBR .~ 0.1)
 
 draw :: Diagram PGF -> IO ()
-draw = defaultMain . pad 1.1
+draw = defaultMain . pad 1.1 . scale 3
